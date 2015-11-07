8316b279dfa6680e4912e89105e46a72db93d268
Blake Shaw <blake.shaw@gmail.com>
2015-11-07 10:04:33 -0800

    initial commit of a sample practice program

---
+++ Factor.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Program {
+	static void Main() {
+		Console.WriteLine("Welcome to Factor!");
+		Console.Write("Please enter a positive integer: ");
+		var input = Console.ReadLine().Trim();
+		int number;
+		if(!int.TryParse(input, out number)) {
+			Console.WriteLine("Could not parse input.");
+			Console.WriteLine("Goodbye.");
+			return;
+		}
+		if(number < 1){
+			Console.WriteLine("...a POSITIVE integer!");
+			Console.WriteLine("Goodbye.");
+			return;
+		}
+		Console.WriteLine("Factors of " + number + ":");
+		for (int ii = 1; ii <= number; ii++)
+		{
+			if(IsFactor(number,ii)) Console.Write(ii + " ");
+		}
+	}
+	
+	static bool IsFactor(int number, int factor) {
+		for(int ii = 1; ii <= number; ii++)
+		{
+			if(ii * factor == number) return true;
+		}
+		return false;
+	}
+}
