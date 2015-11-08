134376810b49b451172ddf07321e74aa1e0c77f1
Blake Shaw <blake.shaw@gmail.com>
2015-11-08 01:30:01 -0800

    added an lru cache class with a simple test program.

---
+++ Cache.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main()
+	{
+		var cache = new LruCache<int,int>(getNum, 5);
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine(cache.GetItem(i));
+		}
+		for (int i = 9; i >= 0; i--)
+		{
+			Console.WriteLine(cache.GetItem(i));
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine(cache.GetItem(i));
+		}
+	}
+	
+	static int getNum(int num)
+	{
+		Console.WriteLine("cache miss");
+		return num;
+	}
+}
+
+class LruCache<TKey, TVal>
+{
+	int _cacheSize;
+	Func<TKey, TVal> _getUncached;
+	Dictionary<TKey, TVal> _cache;
+	List<Tuple<TKey, DateTime>> _accessTimes;
+	
+	public LruCache(Func<TKey, TVal> getUncached, int cacheSize)
+	{
+		_cacheSize = cacheSize;
+		_getUncached = getUncached;
+		Clear();
+	}
+	
+	public void Clear()
+	{
+		_cache = new Dictionary<TKey, TVal>();
+		_accessTimes = new List<Tuple<TKey, DateTime>>();
+	}
+	
+	private void trimCache()
+	{
+		if(_cache.Count > _cacheSize)
+		{
+			_accessTimes.Sort((y,x) => x.Item2.CompareTo(y.Item2));
+			var leastUsed = _accessTimes[_accessTimes.Count - 1];
+			_cache.Remove(leastUsed.Item1);
+			_accessTimes.Remove(leastUsed);
+		}
+	}
+	
+	public TVal GetItem(TKey key)
+	{
+		TVal item;
+		if(!_cache.TryGetValue(key, out item))
+		{
+			 item = _getUncached(key);
+			 _cache.Add(key, item);
+		}
+		_accessTimes.RemoveAll(x => x.Item1.Equals(key));
+		_accessTimes.Add(Tuple.Create(key, DateTime.Now));
+		trimCache();
+		return item;
+	}
+}
--- build.bat
+++ build.bat
@@ -2,7 +2,9 @@ cls
 csc Factor.cs ArgParse.cs
 csc Fibonacci.cs ArgParse.cs
 csc StringCompress.cs
+csc Cache.cs
 
 Factor 1000
 Fibonacci 1000
 StringCompress "This is 333 55555 aaa tesssssstttttt 232."
+Cache
