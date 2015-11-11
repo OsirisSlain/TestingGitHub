c1da4c5b7090edaf93308a45cf2a87808d8b4065
Blake Shaw <blake.shaw@gmail.com>
2015-11-10 17:59:04 -0800

    missed the main file last commit...

--- BuildInterview.cs
+++ BuildInterview.cs
@@ -1,41 +1,97 @@
 namespace Question
 {
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-	static void Main()
+	using System;
+	using System.Collections.Generic;
+	
+	class Program
 	{
-		int[] sample = {11, 4, 3, 3, 6, 3, 7, 2, 9};
-		var nextLargest = NextLargestFast(sample);
-		foreach (var item in nextLargest)
+		static void Main()
 		{
-			Console.Write(item + " ");
+			int[] sample = {11, 4, 3, 3, 6, 3, 7, 2, 9};
+			int[] expected = {-1, 3, 2, 1, 2, 1, 2, 1, -1};
+			
+			Console.WriteLine("Input");
+			WriteLine(sample);
+			
+			Console.WriteLine("Expected");
+			WriteLine(expected);
+			
+			var stackResult = NextLargest_Stack(sample);
+			VerboseVerify(expected, stackResult, "Stack");
+			
+			var loopResult = NextLargest_Loop(sample);
+			VerboseVerify(expected, loopResult, "Loop");
 		}
-	}
-	
-	//stack based solution
-	static int[] NextLargestFast(int[] original)
-	{
-		int[] result = new int[original.Length];
-		var index = new Stack<int>();
-		//Func<int> top = () => index.Peek();
-	
-		for(int i = 0; i < original.Length; i++)
+		
+		static void WriteLine(int[] a)
 		{
-			while(index.Count > 0 && original[i] > original[index.Peek()])
+			Console.WriteLine(String.Join(", ", a));
+		}
+		
+		static bool VerboseVerify(int[] expected, int[] actual, string testName)
+		{
+			var result = Verify(expected, actual);
+			
+			if(result)
+				Console.WriteLine(testName + " results match expected.");
+			else
 			{
-				result[index.Peek()] = i - index.Peek();
-				index.Pop();
+				Console.WriteLine(testName + " results don't match expected:");
+				WriteLine(actual);
 			}
-			index.Push(i);
+				
+			return result;
 		}
-		while(index.Count > 0)
+		
+		static bool Verify(int[] expected, int[] actual)
 		{
-			result[index.Pop()] = -1;
+			for(int i = 0; i < expected.Length; i++)
+			{
+				if(expected[i] != actual[i])
+					return false;
+			}
+			return true;
 		}
-		return result;
+		
+		//stack based solution
+		static int[] NextLargest_Stack(int[] original)
+		{
+			int[] result = new int[original.Length];
+			var index = new Stack<int>();
+		
+			for(int i = 0; i < original.Length; i++)
+			{
+				while(index.Count > 0 && original[i] > original[index.Peek()])
+				{
+					var top = index.Pop();
+					result[top] = i - top;
+				}
+				index.Push(i);
+			}
+			while(index.Count > 0)
+			{
+				result[index.Pop()] = -1;
+			}
+			return result;
+		}
+		
+		//simple nested loop solution
+		static int[] NextLargest_Loop(int[] original)
+		{
+			int[] result = new int[original.Length];
+			for(int i = 0; i < original.Length; i++)
+			{
+				result[i] = -1;
+				for(int j = 1; j < original.Length - i; j++)
+				{
+					if(original[i] < original[i+j])
+					{
+						result[i] = j;
+						break;
+					}
+				}
+			}
+			return result;
+		}
 	}
-}
 }
